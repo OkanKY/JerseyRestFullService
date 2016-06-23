70f9f40313e6a0cfdb93670a831a68da9583e097
Okan Kaya <okan93kaya@gmail.com>
2016-06-23 11:18:07 +0300

    android upload

---
+++ embedded/arduino.ino
@@ -0,0 +1,90 @@
+
+#include "DHT.h"
+
+#define DHTPIN 2     // what digital pin we're connected to
+
+// Uncomment whatever type you're using!
+//#define DHTTYPE DHT11   // DHT 11
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+// to 3.3V instead of 5V!
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+// Initialize DHT sensor.
+// Note that older versions of this library took an optional third parameter to
+// tweak the timings for faster processors.  This parameter is no longer needed
+// as the current DHT reading algorithm adjusts itself to work on faster procs.
+DHT dht(DHTPIN, DHTTYPE);
+char data = 0;                //Variable for storing received data
+int timer=0;
+void setup() 
+{
+  Serial.begin(9600);         //Sets the data rate in bits per second (baud) for serial data transmission
+  pinMode(13, OUTPUT);        //Sets digital pin 13 as output pin
+  dht.begin();
+  showTempHum();  
+}
+void loop()
+{
+  
+  if(Serial.available() > 0)  // Send data only when you receive data:
+  {
+    data = Serial.read();      //Read the incoming data and store it into variable data
+    Serial.print(data);        //Print Value inside data in Serial monitor
+    Serial.print("\n");        //New line 
+    if(data == '1')            //Checks whether value of data is equal to 1 
+      digitalWrite(13, HIGH);  //If value is 1 then LED turns ON
+    else if(data == '0')       //Checks whether value of data is equal to 0
+      digitalWrite(13, LOW);   //If value is 0 then LED turns OFF
+  }   
+  timer++;
+  if(timer>10000)
+  {
+  showTempHum();  
+  timer=0;
+  }                       
+ 
+}
+
+void showTempHum() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print(f);
+  Serial.print(" *F\t");
+  Serial.print("Heat index: ");
+  Serial.print(hic);
+  Serial.print(" *C ");
+  Serial.print(hif);
+  Serial.println(" *F");
+}
